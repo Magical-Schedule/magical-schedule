4098c869b21c3935a4345b6c9779b3ed5abc21fa
Mark Vrčko <mark@flashnet.xyz>
2025-12-21 16:47:26 +0100

    big bang

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+class_name Player extends CharacterBody2D
+
+var move_speed:float = 350.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var direction: Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("move_right") -  Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") -  Input.get_action_strength("move_up")
+	
+	velocity = move_speed * direction
+	
+	pass
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
