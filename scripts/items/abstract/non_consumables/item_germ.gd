f66834a83b354e05f501a0c3de4a9cf8f692fee3
Aleksandar Temelkov <temelkovaleksa@gmail.com>
2025-12-26 00:03:00 +0100

    [WIP] V1 Razredov za Iteme
    • Prva različica razredov za Iteme – le abstraktni razredi definirani, brez konkretnih razredov oziroma dejanskih itemov

---
+++ scripts/items/abstract/non_consumables/item_germ.gd
@@ -0,0 +1,2 @@
+# GermItem - abstract class for germ items
+class_name GermItem extends NonConsumableItem
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,8 +5,11 @@ var move_speed: float = 350.0
 
 func _ready() -> void:
 	pass
+	
+func _process(_delta: float) -> void:
+	pass
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
