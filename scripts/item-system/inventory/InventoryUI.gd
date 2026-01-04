9a1af32bf64c9b7d690a8af06ce98e24e1c64d30
jakob-jereb-sola <jakob.jereb@student.um.si>
2026-01-04 20:09:12 +0100

    Inventory grid added
    Created the grid design and the initial fill of null itemSlots.

--- scripts/item-system/inventory/InventoryUI.gd
+++ scripts/item-system/inventory/InventoryUI.gd
@@ -2,15 +2,20 @@ extends Control
 
 @export var slot_scene: PackedScene
 @onready var inv_grid = $Panel/VBoxContainer/InventoryGrid
-@onready var hotbar_grid = $Panel/VBoxContainer/HotbarGrid
+@onready var hotbar_grid = $Panel2/VBoxContainer/HotbarGrid
+
+@onready var inv_panel = $Panel
+@onready var hotbar_panel = $Panel2
+
 
 var inventory_data: InventoryData
 
 func _ready():
 	print("creating inventory")
 	inventory_data = InventoryData.load_or_create()
-	visible = false 
-	#populate_grids()
+	inv_panel.visible = false 
+	hotbar_panel.visible = true
+	populate_grids()
 #naredi inventory
 func populate_grids():
 	for child in inv_grid.get_children(): child.queue_free()
@@ -26,7 +31,10 @@ func populate_grids():
 #inventory toggle z E
 func _input(event):
 	if event.is_action_pressed("inventory_toggle"): 
-		visible = !visible
+		if inv_panel.visible:
+			inv_panel.visible = false
+		else:
+			inv_panel.visible = true
 		if visible:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
