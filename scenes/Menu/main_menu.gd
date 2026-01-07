874b7016536e66b3de18929b2103ac1cae03d04c
blaz <blaz.bocek@gmail.com>
2026-01-07 21:32:54 +0100

    Restored fade when transitioning to level

--- scenes/Menu/main_menu.gd
+++ scenes/Menu/main_menu.gd
@@ -3,31 +3,43 @@ extends Control
 var playScene : String = "res://Scenes/dev/playground.tscn"
 
 @export var fade_time := 1.5
+@export var fade_out := 0.5
 
 @onready var image_a: NinePatchRect = $FadedBg
 @onready var image_b: NinePatchRect = $IconBg
+@onready var fade: ColorRect = $Fade
 
 func _ready():
+	fade.modulate.a = 0.0
+	fade.visible = true
 	image_b.modulate.a = 0.0
+	
 	await get_tree().create_timer(0.5).timeout
 	fade_to_image_b()
 
 func fade_to_image_b():
 	var tween := create_tween()
 	tween.set_parallel(true)
-	
 	tween.tween_property(image_a, "modulate:a", 0.0, fade_time)
 	tween.tween_property(image_b, "modulate:a", 0.8, fade_time)
 
-
-func _process(delta: float) -> void:
-	pass
-
 func _on_quit_pressed() -> void:
-	print("Exit pressed")
 	get_tree().quit()
 
-
 func _on_play_pressed() -> void:
-	#GF oz. GameFlow je global script ki ureja tranzicije med scenami in podobne zadeve.
-	GF.load_scene(playScene)
+	fade_then_load_scene(playScene)
+
+func fade_then_load_scene(scene_path: String) -> void:
+	fade.modulate.a = 0.0
+	fade.visible = true
+	
+	await get_tree().process_frame
+	
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(fade, "modulate:a", 1.0, fade_out)
+	
+	await tween.finished
+	
+	GF.load_scene(scene_path)
