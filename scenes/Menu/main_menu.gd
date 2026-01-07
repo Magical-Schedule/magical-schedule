fe1a16c26b849d41b1023bacbfd9a39afe4dba92
blaz <blaz.bocek@gmail.com>
2026-01-07 17:55:25 +0100

    Added logo animations, changed background fade to be more subtle

--- scenes/Menu/main_menu.gd
+++ scenes/Menu/main_menu.gd
@@ -2,14 +2,14 @@ extends Control
 
 var playScene : String = "res://Scenes/dev/playground.tscn"
 
-@export var fade_time := 2.0
+@export var fade_time := 1.5
 
 @onready var image_a: NinePatchRect = $FadedBg
 @onready var image_b: NinePatchRect = $IconBg
 
 func _ready():
 	image_b.modulate.a = 0.0
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.5).timeout
 	fade_to_image_b()
 
 func fade_to_image_b():
@@ -17,7 +17,7 @@ func fade_to_image_b():
 	tween.set_parallel(true)
 	
 	tween.tween_property(image_a, "modulate:a", 0.0, fade_time)
-	tween.tween_property(image_b, "modulate:a", 1.0, fade_time)
+	tween.tween_property(image_b, "modulate:a", 0.8, fade_time)
 
 
 func _process(delta: float) -> void:
--- scenes/Menu/panel.gd
+++ scenes/Menu/panel.gd
@@ -6,22 +6,19 @@ extends Control
 
 var start_pos: Vector2
 
+@export var float_distance := 10.0
+@export var float_time := 1.5
+
 func _ready():
 	start_pos = position
-
 	position += offscreen_offset
-
 	await get_tree().process_frame
-	
 	modulate.a = 0.0
-
 	await get_tree().process_frame
-	
-	await get_tree().create_timer(3.0).timeout
-
+	await get_tree().create_timer(2.0).timeout
 	fade_in()
-	
 	slide_in()
+	start_floating()
 
 func fade_in():
 	var tween := create_tween()
@@ -36,3 +33,13 @@ func slide_in():
 	tween.set_ease(Tween.EASE_OUT)
 
 	tween.tween_property(self, "position", start_pos, slide_time)
+	
+
+func start_floating():
+	var tween = create_tween()
+	tween.set_loops()  # infinite loop
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
+
+	tween.tween_property(self, "position:y", start_pos.y - float_distance, float_time)
+	tween.tween_property(self, "position:y", start_pos.y + float_distance, float_time)
