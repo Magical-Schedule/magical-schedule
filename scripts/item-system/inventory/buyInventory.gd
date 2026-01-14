f1a53ea1ff3752d5a1d0e59c9328f676c4a49957
jakob-jereb-sola <jakob.jereb@student.um.si>
2026-01-15 00:15:02 +0100

    buying logic implementet

--- scripts/item-system/inventory/buyInventory.gd
+++ scripts/item-system/inventory/buyInventory.gd
@@ -1,11 +1,17 @@
-extends Node
+extends Control
 
+@onready var box = $PanelContainer/HBoxContainer
+var inUi 
+func _ready():
+	box.get_node("TextureButton").pressed.connect(_on_buy_clicked.bind("blue_flower_seed"))
+	box.get_node("TextureButton2").pressed.connect(_on_buy_clicked.bind("red_flower_seed"))
+	box.get_node("TextureButton3").pressed.connect(_on_buy_clicked.bind("yellow_flower_seed"))
+	box.get_node("TextureButton4").pressed.connect(_on_buy_clicked.bind("green_flower_seed"))
+	box.get_node("TextureButton5").pressed.connect(_on_buy_clicked.bind("white_flower_seed"))
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_buy_clicked(item_name: String):
+	inUi =  get_tree().root.find_child("InventoryUi", true, false)
+	var path = "res://scripts/items/Define/" + item_name + ".tres"
+	var item = load(path)
+	if item:
+		inUi.add_item(item)
