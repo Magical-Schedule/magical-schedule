14fe1fff774de963b3a90824ceefeb7d85f96d0e
jakob-jereb-sola <jakob.jereb@student.um.si>
2026-01-15 10:00:27 +0100

    Buying logic and menu added

--- scripts/item-system/inventory/buyInventory.gd
+++ scripts/item-system/inventory/buyInventory.gd
@@ -4,14 +4,20 @@ extends Control
 var inUi 
 func _ready():
 	box.get_node("TextureButton").pressed.connect(_on_buy_clicked.bind("blue_flower_seed"))
-	box.get_node("TextureButton2").pressed.connect(_on_buy_clicked.bind("red_flower_seed"))
-	box.get_node("TextureButton3").pressed.connect(_on_buy_clicked.bind("yellow_flower_seed"))
-	box.get_node("TextureButton4").pressed.connect(_on_buy_clicked.bind("green_flower_seed"))
-	box.get_node("TextureButton5").pressed.connect(_on_buy_clicked.bind("white_flower_seed"))
+	box.get_node("TextureButton2").pressed.connect(_on_buy_clicked.bind("mushroom_seed"))
+	box.get_node("TextureButton3").pressed.connect(_on_buy_clicked.bind("fly_seed"))
+	box.get_node("TextureButton4").pressed.connect(_on_buy_clicked.bind("grass_seed"))
+	box.get_node("TextureButton5").pressed.connect(_on_buy_clicked.bind("tree_seed"))
+
 
 func _on_buy_clicked(item_name: String):
+	var play = get_tree().root.find_child("Playground", true, false)
 	inUi =  get_tree().root.find_child("InventoryUi", true, false)
 	var path = "res://scripts/items/Define/" + item_name + ".tres"
 	var item = load(path)
+	if item.price <=play.get_money():
+		play.sub_money(item.price)
+	else:
+		return
 	if item:
 		inUi.add_item(item)
