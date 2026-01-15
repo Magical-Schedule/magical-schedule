b109e6ee1edbd944b8ad99699011806510d7c730
jakobrajsp <jakobrajsp19@gmail.com>
2026-01-15 11:57:14 +0100

    Added all planting sprites

--- scripts/crop.gd
+++ scripts/crop.gd
@@ -1,18 +1,17 @@
 extends Resource
 class_name Crop
 
-# Osnovno
-@export var crop_name: String = "blue_plant"
-@export var growth_stages: int = 4  # koliko faz rasti
-@export var time_per_stage: float = 13.0  # sekunde za vsako fazo (za debug)
+@export var crop_name: String = "" 
+@export var growth_stages: int = 4  
+@export var time_per_stage: float = 5.0 # Hitrejše za testiranje
 
-# Trenutno stanje
-var current_stage: int = 0  # 0 = ravno posajeno, 3 = zrelo (pri 4 fazah)
+var current_stage: int = 0  
 var growth_timer: float = 0.0
 
-# Yield
 @export var base_yield: int = 2
-@export var harvest_item_name: String = "blue_flower"
+@export var harvest_item_name: String = "" # Prazno, da ga Field.gd prisili v pravilno ime
+
+
 
 func is_mature() -> bool:
 	return current_stage >= growth_stages - 1
--- scripts/item-system/inventory/ItemData.gd
+++ scripts/item-system/inventory/ItemData.gd
@@ -7,3 +7,6 @@ class_name ItemData
 @export var placable: bool
 @export var plantable: bool
 @export var price: int = 10
+@export var is_seed: bool = false
+@export var crop_row_index: int = 0
+@export var growth_spritesheet: Texture2D
