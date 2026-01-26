e5bbb5016cba6cd998a6b56ab593af7e1330b005
Aleksandar Temelkov <temelkovaleksa@gmail.com>
2026-01-26 15:59:25 +0100

    [Sync] Preparation for merging with game_with_added_history

--- scripts/item-system/inventory/buyInventory.gd
+++ scripts/item-system/inventory/buyInventory.gd
@@ -1,23 +1,24 @@
 extends Control
 
 @onready var box = $PanelContainer/HBoxContainer
-var inUi 
-func _ready():
-	box.get_node("TextureButton").pressed.connect(_on_buy_clicked.bind("blue_flower_seed"))
-	box.get_node("TextureButton2").pressed.connect(_on_buy_clicked.bind("mushroom_seed"))
-	box.get_node("TextureButton3").pressed.connect(_on_buy_clicked.bind("fly_seed"))
-	box.get_node("TextureButton4").pressed.connect(_on_buy_clicked.bind("grass_seed"))
-	box.get_node("TextureButton5").pressed.connect(_on_buy_clicked.bind("tree_seed"))
+var inUi
 
+func _ready():
+	box.get_node("TextureButton").pressed.connect(_on_buy_clicked.bind("seed_flower"))
+	box.get_node("TextureButton2").pressed.connect(_on_buy_clicked.bind("seed_flytrap"))
+	box.get_node("TextureButton3").pressed.connect(_on_buy_clicked.bind("seed_grass"))
+	box.get_node("TextureButton4").pressed.connect(_on_buy_clicked.bind("seed_mushroom"))
+	box.get_node("TextureButton5").pressed.connect(_on_buy_clicked.bind("seed_tree"))
 
 func _on_buy_clicked(item_name: String):
 	var play = get_tree().root.find_child("Playground", true, false)
 	inUi =  get_tree().root.find_child("InventoryUi", true, false)
-	var path = "res://scripts/items/Define/" + item_name + ".tres"
-	var item = load(path)
-	if item.price <=play.get_money():
+	var path = "res://data/items/seeds/" + item_name + ".tres"
+	var item: Item = load(path)
+	
+	if item.price <= play.get_money():
 		play.sub_money(item.price)
-	else:
-		return
+	else: return
+	
 	if item:
 		inUi.add_item(item)
